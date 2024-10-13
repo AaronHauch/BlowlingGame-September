a8b7ab9dae188de011f04bf973765fbdbfc48210
AaronHauch <aaronhauch1031@gmail.com>
2024-10-13 16:03:02 -0400

    Class 8

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,69 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    [SerializeField] private Pin[] pins;
+    [SerializeField] private int throwCounter;
+    [SerializeField] private GameObject ballPrefab;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartThrow();
+        
+
+    }
+
+   public void BallOnPit()
+    {
+        throwCounter++;
+
+      
+        Invoke("StartThrow", 3);
+
+
+    }
+
+    // Update is called once per frame
+   public void StartThrow()
+    {
+        CheckForFallenPins();
+
+        if (throwCounter == 2)
+        {
+            RepositionPins();
+            throwCounter = 0;
+        }
+
+      
+        Instantiate(ballPrefab, transform.position, transform.rotation);
+    }
+
+    void RepositionPins()
+    {
+        foreach(Pin p in pins)
+        {
+            p.gameObject.SetActive(true);
+            p.ResetPinToOrgin();
+        }
+    }
+
+    void CheckForFallenPins()
+    {
+        int score = 0;
+
+        foreach(Pin pin in pins)
+        {
+            if(pin.IsPinFallen())
+            {
+                score++;
+                //if (pin.gameObject.activeInHierarchy)
+                pin.gameObject.SetActive(false);
+            }
+      
+        }
+        Debug.Log(score);
+    }
+}
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Diagnostics;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
@@ -9,24 +10,58 @@ public class PlayerController : MonoBehaviour
     [SerializeField] private Rigidbody myRigidbody;
     [SerializeField] private GameObject aimingArrow;
 
-    // Start is called before the first frame update
-    void Start()
+     // Update is called once per frame
+   private void Update()
     {
-          
+        MoveBall();
+        ThrowingBall();
+        
     }
 
-    // Update is called once per frame
-    void Update()
+   private void MoveBall()
     {
-        transform.Translate(Input.GetAxis("Horizontal") * Time.deltaTime, 0, 0);
-    
-        if (Input.GetKeyDown(KeyCode.Space) && wasThrown == false)
+        if (!wasThrown) 
         {
-            aimingArrow.SetActive(false);
-            wasThrown = true;
-            myRigidbody.AddForce(aimingArrow.transform.forward * throwStrength, ForceMode.Impulse);
+            transform.Translate(Input.GetAxis("Horizontal") * Time.deltaTime, 0, 0);
         }
         
+    }
+
+    void ThrowingBall()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) && wasThrown == false)
+        {
+        
+            aimingArrow.SetActive(false);
             
+            wasThrown = true;
+            myRigidbody.AddForce(aimingArrow.transform.forward * throwStrength, ForceMode.Impulse);
+            Invoke("StopThrow", 10f);
+        }
+    }
+
+   
+
+    void OnCollisionEnter(Collision collision)
+    {
+        
+        if (collision.gameObject.CompareTag("Pin"))
+        {
+            
+        }
+        
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        StopThrow();
+    }
+    
+    void StopThrow()
+    {
+        FindObjectOfType<GameManager>().BallOnPit();
+        Destroy(gameObject, 3f);
     }
 }
+
+    
